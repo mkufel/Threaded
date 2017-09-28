87414888588f3c6c15926de72bc380307bd3df67
Ahmed Ahres <ahmedahres98@gmail.com>
2017-09-28 11:08:25 +0200

    project files

---
+++ cmake-build-debug/CMakeFiles/Threaded.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Threaded.dir/flip.c.o"
+  "CMakeFiles/Threaded.dir/threaded_basics.c.o"
+  "Threaded.pdb"
+  "Threaded"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Threaded.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
