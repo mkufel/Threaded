6c67b7c4bd823088b49c6b4c89e07a8dcdf3eb4b
Maciej Kufel <kufcio97@gmail.com>
2017-10-03 20:52:23 +0200

    Working version

--- flip.c
+++ flip.c
@@ -20,15 +20,12 @@
 #include <pthread.h>
 #include <unistd.h>
 
-// create a bitmask where bit at position n is set
-#define BITMASK          ((uint128_t) 0xffffffffffffffff)
 
 // check if bit n in v is set
 #define BIT_IS_SET(v,n)     (((v) & BITMASK(n)) == BITMASK(n))
 
-static pthread_mutex_t      mutex          = PTHREAD_MUTEX_INITIALIZER;
-
-static bool * isThreadInitialized = true;
+static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+static pthread_mutex_t isThreadInitialized = PTHREAD_MUTEX_INITIALIZER;
 
 void printBits(size_t const size, void const * const ptr)
 {
@@ -47,6 +44,26 @@ void printBits(size_t const size, void const * const ptr)
     puts("\n");
 }
 
+
+/*
+ * rsleep(int t)
+ *
+ * The calling thread will be suspended for a random amount of time
+ * between 0 and t microseconds
+ * At the first call, the random generator is seeded with the current time
+ */
+static void rsleep (int t)
+{
+    static bool first_call = true;
+
+    if (first_call == true)
+    {
+        srandom (time(NULL) % getpid());
+        first_call = false;
+    }
+    usleep (random () % t);
+}
+
 static void *
 do_flip(void * arg) {
 
@@ -59,9 +76,9 @@ do_flip(void * arg) {
     argi = (int *) arg;     // proper casting before dereferencing (could also be done in one statement)
     param = *argi;              // get the integer value of the pointer
 //    printf("Pointer to arg: %p, pointer to argi: %p, pointer to i %p\n", arg, argi, (&param));
-    isThreadInitialized = true;
+    pthread_mutex_unlock (&isThreadInitialized);
 
-    for (int i = 2; i < NROF_PIECES; i++) {
+    for (int i = 2; i <= NROF_PIECES; i++) {
 
         if(i % param == 0)
         {
@@ -72,8 +89,10 @@ do_flip(void * arg) {
 
 //            printf("Before with param: %d and a piece: %d\n", param, i);
 //            printBits(sizeof(buffer[buffer_index]), &buffer[buffer_index]);
+//            printf("Flipping bit nr: %d as a multiple of %d\n", i, param);
 
             buffer[buffer_index] ^= (uint128_t) 1 << bit_index-1;
+//            printf("After shift with param: %d, iteration: %d\n", param, i);
 
 //            printf("\n After: ");
 //            printBits(sizeof(buffer[buffer_index]), &buffer[buffer_index]);
@@ -87,6 +106,9 @@ do_flip(void * arg) {
 
 int main (void)
 {
+    for (int l = 0; l < sizeof(buffer)/16; ++l) {
+        buffer[l] = ~ 0;
+    }
 
     pthread_t   my_threads[NROF_THREADS];   //array of thread id's
 
@@ -97,19 +119,10 @@ int main (void)
 
     for (int i = 0; i < NROF_THREADS; i++) {
 
-        if (isThreadInitialized)
-        {
+            pthread_mutex_lock (&isThreadInitialized);
             *parameter += 1;                                            //increase the parameter
             pthread_create(&my_threads[i], NULL, do_flip,
                            parameter);    //make a thread to the flipping with a certain parameter
-            isThreadInitialized = false;
-        }
-        else if (i < NROF_THREADS)
-        {
-
-            i--;
-        }
-        else break;
     }
 
     for (int j = 0; j < sizeof(my_threads)/8; j++) 					//wait for all threads to terminate
@@ -122,12 +135,12 @@ int main (void)
 
     printBits(sizeof(uint128_t), &mask);
 
-    for (int k = 0; k < sizeof(buffer)/sizeof(uint16_t); k++) {
-        for (int i = 0; i < sizeof(uint128_t); i++) {
+    for (int k = 0; k < sizeof(buffer)/16; k++) {
+        for (int i = 0; i < 128; i++) {
             if (buffer[k]  & mask << i
             && ((k*128) + i) < NROF_PIECES)
             {
-                printf("%d, ", (k*128) + i);
+                printf("%d, ", (k*128) + i + 1);
             }
         }
     }
